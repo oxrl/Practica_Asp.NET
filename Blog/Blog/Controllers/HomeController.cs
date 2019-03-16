7e99f670a19132b29cae39cb664a188cc8b30784
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2019-03-16 04:11:46 -0500

    Creacion de una Funcion en home y su vista

--- Blog/Blog/Controllers/HomeController.cs
+++ Blog/Blog/Controllers/HomeController.cs
@@ -8,6 +8,10 @@ namespace Blog.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult MyPage()
+        {
+            return View();
+        }
         public ActionResult Index()
         {
             return View();
